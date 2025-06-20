d226a4a7db58c5c000a944bad7d1c7a73183a00b
luongpham <psluong952@gmail.com>
2025-06-20 20:36:22 +0700

    Ecommerce commit: change task user id

--- envs/ecommerce/interface_4_tasks.py
+++ envs/ecommerce/interface_4_tasks.py
@@ -3,7 +3,7 @@ from tau_bench.types import Action, Task
 INTERFACE_4_TEST = [
     Task(
         annotator="0",
-        user_id="USR003",
+        user_id="USR821",
         instruction="User USR821 - email alex.turner821@example.com wants to delivery the shipment for the tracking number TRK302527, order SO0455.",
         actions=[
             Action(
