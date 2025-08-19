59bd0343176e7088a48b1d5e51970862572a1881
Omar Elsendiony <omar.sendiony@gmail.com>
2025-08-19 16:40:04 +0300

    minor_change

--- envs/finance/tools/interface_2/create_investor_subscription.py
+++ envs/finance/tools/interface_2/create_investor_subscription.py
@@ -41,7 +41,7 @@ class CreateInvestorSubscription(Tool):
             "investor_id": investor_id,
             "amount": amount,
             "status": status,
-            "request_assigned_to": "1",  # Default admin
+            "request_assigned_to": "6",  # Default admin
             "request_date": timestamp.split("T")[0],
             "approval_date": timestamp.split("T")[0] if status == "approved" else None,
             "updated_at": timestamp
