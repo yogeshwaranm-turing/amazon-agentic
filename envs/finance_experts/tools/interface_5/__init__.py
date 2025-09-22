ebc6adb6e134035b59c337d1b1e981995bc247f9
Andre Soeiro <andre.s1@turing.com>
2025-09-22 14:16:54 +0000

    Adding APIs by Andre Soeiro

---
+++ envs/finance_experts/tools/interface_5/__init__.py
@@ -0,0 +1,9 @@
+from .generate_report import GenerateReport
+from .manage_instrument import ManageInstrument
+from .process_redemption import ProcessRedemption
+
+ALL_TOOLS_INTERFACE_5 = [
+    GenerateReport,
+    ManageInstrument,
+    ProcessRedemption
+]
