6b786110720558b15460954e92b1b6ae09d0cc69
israelodeajo <israel.o@turing.com>
2025-10-16 13:45:26 -0700

    interface1 tools updated __init__.py

--- envs/hr_talent_management/tools/interface_1/__init__.py
+++ envs/hr_talent_management/tools/interface_1/__init__.py
@@ -1,5 +1,4 @@
-
-
+# interface_1/tool_registry.py
 from .discover_candidate_entities import DiscoverCandidateEntities
 from .discover_interview_offer_entities import DiscoverInterviewOfferEntities
 from .manage_employee_operations import ManageEmployeeOperations
@@ -12,6 +11,12 @@ from .manage_payroll_earning_operations import ManagePayrollEarningOperations
 from .manage_payslip_operations import ManagePayslipOperations
 from .discover_payroll_entities import DiscoverPayrollEntities
 from .discover_benefit_entities import DiscoverBenefitEntities
+from .discover_reference_entities import DiscoverReferenceEntities
+from .discover_job_entities import DiscoverJobEntities
+from .manage_job_operations import ManageJobOperations
+from .manage_candidate_operations import ManageCandidateOperations
+from .manage_application_operations import ManageApplicationOperations
+from .manage_interview_operations import ManageInterviewOperations
 from .discover_employee_entities import DiscoverEmployeeEntities
 from .discover_document_task_entities import DiscoverDocumentTaskEntities
 from .manage_document_operations import ManageDocumentOperations
@@ -31,6 +36,12 @@ ALL_TOOLS_INTERFACE_1 = [
     ManagePayslipOperations,
     DiscoverPayrollEntities,
     DiscoverBenefitEntities,
+    DiscoverReferenceEntities,
+    DiscoverJobEntities,
+    ManageJobOperations,
+    ManageCandidateOperations,
+    ManageApplicationOperations,
+    ManageInterviewOperations,
     DiscoverEmployeeEntities,
     DiscoverDocumentTaskEntities,
     ManageDocumentOperations,
